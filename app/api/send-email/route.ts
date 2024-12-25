95ee4395a1750a409c6bf056a1fb3884756f4952
Mustafa <mustafabhikhpurwala@gmail.com>
2024-12-25 23:17:59 +0530

    form fixes

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -4,6 +4,7 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export async function POST(req: Request) {
+  if(req.method == 'POST') {
   try {
     const data = await req.json();
     
@@ -43,4 +44,8 @@ export async function POST(req: Request) {
     console.error('Error sending email:', error);
     return NextResponse.json({ error: 'Failed to send email' }, { status: 500 });
   }
+}
+else{
+  return NextResponse.error();
+}
 }
